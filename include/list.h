5a97b058b4801ad6c86d1644a90f4ce9d166fa8c
brianda-plascencia <brianda_plascencia1@my.vcccd.edu>
2018-11-30 12:29:11 -0800

    testing

--- include/list.h
+++ include/list.h
@@ -4,6 +4,9 @@
 
 #ifndef EX06_LINKEDLIST_LINKEDLIST_H
 #define EX06_LINKEDLIST_LINKEDLIST_H
+
+#include <iostream>
+
 namespace edu { namespace vcccd { namespace vc { namespace csv15 {
     template<class T>
     class LinkedList{
@@ -18,6 +21,7 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
 
         Node *head;
         Node *tail;
+        size_t _size;
 
     public:
         class iterator {
@@ -57,38 +61,45 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
         size_t size() const { return _size; }
 
     public:
-        iterator begin() { return interator(head); }
-        const iterator begin() const { return interator(head); }
-        iterator end() { return interator(nullptr); }
-        const iterator end() const { return interator(nullptr); }
+        iterator begin() { return iterator(head); }
+        const iterator begin() const { return iterator(head); }
+        iterator end() { return iterator(nullptr); }
+        const iterator end() const { return iterator(nullptr); }
 
     public:
-        void clear() {}
+        void clear() {
+            erase(begin(), end());
+            delete head;
+            head = nullptr;
+            _size = 0;
+        }
         iterator insert(iterator where, const T &value) {
-            Node<T> *newNode = new Node<T>(val, _next, _prev);
+            Node *node = where.node();
+            Node *newNode = new Node(value, node->_next, node->_prev);
 
-            newNode -> _next = n -> next;
-            newNode -> _prev = n;
-            n -> _next = newNode;
-            n -> next -> _prev = newNode;
+            newNode -> _next = node -> _next;
+            newNode -> _prev = node;
+            node -> _next -> _prev = newNode;
+            node -> _next = newNode;
+            return where;
         }
         //iterator erase(iterator where) {}
         iterator erase(iterator first, iterator last) {
+            iterator iter = first;
             for(auto i = first; i != last; i++) {
-                erase(i);
+                iter = erase(i);
             }
+            return iter;
         }
         void push_back(const T &value) {
             insert(tail, value);
         }
         void pop_back() {
-            std::cout(tail);
             erase(tail);
         }
         void push_front(const T &value) {
             insert(head, value);}
         void pop_front() {
-            std::cout(head);
             erase(head);
         }
 
@@ -96,7 +107,7 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
         void swap(LinkedList &other) {
             std::swap(this -> head, other.head);
             std::swap(this -> tail, other.tail);
-            std::swap(this -> size, other.size);
+            std::swap(this -> _size, other._size);
         }
 
 /*    private:
@@ -105,26 +116,23 @@ namespace edu { namespace vcccd { namespace vc { namespace csv15 {
         size_t _size;
 */
     public:
-        void erase(iterator where){
-            Node<T> *n = iterator.node();
-            if (head == nullptr && tail == nullptr) return;
-
+        iterator erase(iterator where){
+            Node *n = where.node();
+            if (head == nullptr && tail == nullptr) return where;
             if (n -> _prev != nullptr) {
-                n -> prev -> _next = n -> next;
+                n -> _prev -> _next = n -> _next;
             }
             else {
-                head = n -> next;
+                head = n -> _next;
             }
-
             if (n -> _next != nullptr) {
-                n -> next -> _prev = n -> prev;
+                n -> _next -> _prev = n -> _prev;
             }
-
             else {
-                tail = n -> prev;
+                tail = n -> _prev;
             }
-
             delete n;
+            return n -> _prev;
         }
 /*
         void insert(Node<T> *n, const T &val){
